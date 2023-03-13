95bc1453013b350b2beccac84f9ec1b5f582de55
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2023-03-13 16:09:59 -0400

    testing trigger

--- 01_check_tax_names/01_script.R
+++ 01_check_tax_names/01_script.R
@@ -6,7 +6,9 @@ occ <- read.csv(here::here("data_table.csv"), sep = ",")
 
 # searching species names in CAS ------------------------------------------
 spp <- unique(occ$spp)
+
 spp_names <- name_backbone_checklist(spp)
+write.csv(x = spp_names, file = here::here("01_check_tax_names", "data.csv"))
 table <- spp_names |>
   knitr::kable()
 
